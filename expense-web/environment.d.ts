04745af3a29ae79f8933958c75b1b695d785d953
Cleber Gomes <cleber@gomes.dev>
2024-11-03 20:33:24 -0300

    feat: create vite project

--- expense-web/environment.d.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-namespace NodeJS {
-  interface ProcessEnv {
-    NODE_ENV: "development" | "production" | "test";
-    NEXT_PUBLIC_API_URL: string;
-  }
-}
--- expense-web/src/components/expenses/expense-form.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import React from "react";
-
-const ExpenseForm: React.FC = () => {
-  return <div>ExpenseForm</div>;
-};
-
-export default ExpenseForm;
