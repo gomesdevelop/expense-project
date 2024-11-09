1fd0f20c1db10a268b7e76ffec4b243fea265d6a
Cleber Gomes <cleber@gomes.dev>
2024-11-09 07:34:05 -0300

    change web project

--- expense-web/next.config.ts
+++ expense-web/next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
-  publicRuntimeConfig: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
 };
 
 export default nextConfig;
--- expense-web/src/app/page.tsx
+++ expense-web/src/app/page.tsx
@@ -1,11 +1,6 @@
 import Image from "next/image";
-import getConfig from "next/config";
-
-const { publicRuntimeConfig } = getConfig();
 
 export default function Home() {
-  console.log("API URL:");
-
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -21,7 +16,7 @@ export default function Home() {
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              {publicRuntimeConfig.NEXT_PUBLIC_API_URL}
+              src/app/page.tsx
             </code>
             .
           </li>
