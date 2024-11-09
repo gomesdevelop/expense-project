b4348500cbce307baa9c1784cfe6ce9c4bb2cfb1
Cleber Gomes <cleber@gomes.dev>
2024-11-09 07:10:15 -0300

    change web project

--- expense-web/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import Dashboard from "@/app/dashboard/page";
-import { createBrowserRouter } from "react-router-dom";
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Dashboard />,
-  },
-]);
